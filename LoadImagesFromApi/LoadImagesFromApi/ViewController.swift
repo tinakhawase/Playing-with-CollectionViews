c13f1b26a090ba28f7917cb7ec0a640d4ccc2746
Tina Khawase <kdeepashri@icloud.com>
2019-04-26 11:49:47 +0200

    updated

--- LoadImagesFromApi/LoadImagesFromApi/ViewController.swift
+++ LoadImagesFromApi/LoadImagesFromApi/ViewController.swift
@@ -13,10 +13,10 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var viewcontroller: UICollectionView!
     
-    // This var is used to store the information from network call
+    
     private var photosFromApi: Welcome
     
-    // Tip: If you want to init a ViewController, use this init(coder aDecoder: NSCoder) type init
+    
     required init?(coder aDecoder: NSCoder) {
         self.photosFromApi = Welcome(page : 0, perPage: 0,photos : [], nextPage : "", prevPage: "")
         super.init(coder: aDecoder)
@@ -25,11 +25,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // tell the tableView that this `self` is the datasource and delegate of it
+        
         self.viewcontroller.dataSource = self as? UICollectionViewDataSource
         self.viewcontroller.delegate = self as? UICollectionViewDelegate
-        // Homework: Add a closure/lambda to this method as a parameter
-        // and do the tableView operations there.
+        
         getSubjects()
     }
 
@@ -51,8 +50,7 @@ class ViewController: UIViewController {
             if let data = data,
                 let serverResponse = try? decoder.decode(Welcome.self, from: data),
                 let response = response as? HTTPURLResponse, 200...299 ~= response.statusCode {
-                // save the serverResponse in self member subjectFromApi
-                // reload the tableView
+                
                 DispatchQueue.main.async { [weak self] in
                     self?.photosFromApi = serverResponse as Welcome
                     self?.viewcontroller.reloadData()
@@ -60,13 +58,7 @@ class ViewController: UIViewController {
             } else {
                 print("Oh noes! error!")
             }
-            /* This code block is for diagnostic purposes, to print the json received
-             guard let json = (try? JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers)) as? [String: Any] else {
-             print("Not containing JSON")
-             return
-             }
-             print(json)
-             */
+            
         }
         
         task.resume()
@@ -79,13 +71,12 @@ class ViewController: UIViewController {
     
     extension ViewController: UICollectionViewDataSource {
         func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            // count of the works from the subjects we received
+            
             return self.photosFromApi.photos.count
         }
         
         func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            // Create a `reusable` tableViewCell for the index no denoted by indexPath param.
-            // How to get the `reusable`cell? Just ask the TableView!
+            
             let photocell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
             let myphotos = photosFromApi.photos[indexPath.row]
             //something that will give access to photographerurl
